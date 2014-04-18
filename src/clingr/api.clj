ea030937b5cc2887bff8a44e36e5f81c351f823d
delihiros <delihiros@gmail.com>
2014-04-18 18:43:02 +0900

    add util

--- src/clingr/api.clj
+++ src/clingr/api.clj
@@ -1,19 +1,16 @@
 (ns clingr.api
-  (:use [clingr.config])
   (:require [clj-http.client :as client]
             [clojure.data.json :as json]))
+
 
 (def url
   "http://lingr.com/api")
 
 (defn lingr [method request]
   (fn [params]
-    (println params)
-    (-> ((condp = method
-           :get client/get 
-           :post client/post)
-         (str url request) 
-         {:query-params params})
-        :body
-        json/read-str)))
+    (-> (client/request
+          {:method method
+           :url (str url request)
+           :query-params params})
+        :body json/read-str)))
 
--- src/clingr/core.clj
+++ src/clingr/core.clj
@@ -1,2 +1,11 @@
-(ns clingr.core)
+(ns clingr.core
+  (:use [clingr.session]))
+
+(def ^:dynamic *session* {})
+
+(defmacro with-session [user & body]
+  `(let [session# (clingr.session/create ~user)]
+     (binding [clingr.core/*session* session#]
+       ~@body)))
+
 
